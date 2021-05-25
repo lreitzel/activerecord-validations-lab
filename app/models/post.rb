861509801db5045aabca5365b9942c1d4116590d
Lydia <lreitzel@gmail.com>
2021-05-25 16:01:40 -0400

    commit tree 37c9ea3ecc7e72edd32b62a2de733a71a7da5ca8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 20}
+    validates :summary, length: {maximum: 100}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :is_clickbait?
+
+    CLICKBAIT = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+    
+    def is_clickbait?
+        if CLICKBAIT.none? { |pat| pat.match title }
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
 end
